41cefdcdf66c0731be1867613a31ed63966874d5
Ryan Cook <cookrn@gmail.com>
2014-11-20 11:23:35 -0700

    some minor updates

--- index.js
+++ index.js
@@ -1,75 +1,123 @@
 (function(){
-  var container = document.getElementById( 'repo_list' )
-    , repo_url  = 'repos.json'
-    , request   = new XMLHttpRequest;
+  var init = function init(){
+    loadProjects();
+    loadRepos();
+  };
 
-  request.open(
-    'GET',
-    repo_url,
-    true
-  );
+  var loadProjects = function loadProjects(){
+    var container   = document.getElementById( 'project_list' )
+      , project_url = 'projects.json'
+      , request     = new XMLHttpRequest;
 
-  request.onload = function(){
-    if( request.status >= 200 && request.status <= 400 ){
-      var repos = JSON.parse( request.responseText )
+    request.open(
+      'GET'
+    , project_url
+    , true
+    );
 
-      repos.forEach( function( repo ){
-        var li         = document.createElement( 'li' )
-          , header     = document.createElement( 'h3' )
-          , link       = document.createElement( 'a' )
-          , badge_list = document.createElement( 'ul' )
-          , desc       = document.createElement( 'p' );
+    request.onload = function(){
+      if( request.status >= 200 && request.status <= 400 ){
+        var projects = JSON.parse( request.responseText )
 
-        badge_list.classList.add( 'badge-list' );
-        header.classList.add( 'repo-header' );
-        li.classList.add( 'repo-list-item' );
-        desc.classList.add( 'repo-description' );
+        projects.forEach( function( project ){
+          var li   = document.createElement( 'li' )
+            , link = document.createElement( 'a' )
+            , desc = document.createTextNode( ': ' + project.description );
 
-        desc.innerText = repo.description;
+          li.classList.add( 'project-list-item' );
 
-        link.setAttribute(
-          'href'
-        , 'https://github.com/' + repo.github_user + '/' + repo.name
-        );
+          link.setAttribute(
+            'href'
+          , project.url
+          );
 
-        if( repo.badges ){
-          repo.badges.forEach( function( badge ){
-            var badge_li        = document.createElement( 'li' )
-              , badge_link      = document.createElement( 'a' )
-              , badge_image     = document.createElement( 'img' )
-              , badge_image_url = badge[ 0 ]
-              , badge_href_url  = badge[ 1 ];
+          link.innerText = project.name;
+          li.appendChild( link );
+          li.appendChild( desc );
+          container.appendChild( li );
+        } );
+      } else {}
+    }
 
-            badge_li.classList.add( 'badge-list-item' );
-            badge_link.classList.add( 'badge-link' );
+    request.onerror = function(){};
+    request.send();
+  };
 
-            badge_link.setAttribute(
-              'href'
-            , badge_href_url
-            );
+  var loadRepos = function loadRepos(){
+    var container = document.getElementById( 'repo_list' )
+      , repo_url  = 'repos.json'
+      , request   = new XMLHttpRequest;
 
-            badge_image.setAttribute(
-              'src'
-            , badge_image_url
-            );
+    request.open(
+      'GET'
+    , repo_url
+    , true
+    );
 
-            badge_link.appendChild( badge_image );
-            badge_li.appendChild( badge_link );
-            badge_list.appendChild( badge_li );
-          } );
-        }
+    request.onload = function(){
+      if( request.status >= 200 && request.status <= 400 ){
+        var repos = JSON.parse( request.responseText )
 
-        link.innerText = 'link';
-        header.innerText = repo.name;
-        li.appendChild( header );
-        li.appendChild( link );
-        li.appendChild( desc );
-        li.appendChild( badge_list );
-        container.appendChild( li );
-      } );
-    } else {}
-  }
+        repos.forEach( function( repo ){
+          var li         = document.createElement( 'li' )
+            , header     = document.createElement( 'h3' )
+            , link       = document.createElement( 'a' )
+            , badge_list = document.createElement( 'ul' )
+            , desc       = document.createElement( 'p' );
 
-  request.onerror = function(){};
-  request.send();
+          badge_list.classList.add( 'badge-list' );
+          header.classList.add( 'repo-header' );
+          li.classList.add( 'repo-list-item' );
+          desc.classList.add( 'repo-description' );
+
+          desc.innerText = repo.description;
+
+          link.setAttribute(
+            'href'
+          , 'https://github.com/' + repo.github_user + '/' + repo.name
+          );
+
+          if( repo.badges ){
+            repo.badges.forEach( function( badge ){
+              var badge_li        = document.createElement( 'li' )
+                , badge_link      = document.createElement( 'a' )
+                , badge_image     = document.createElement( 'img' )
+                , badge_image_url = badge[ 0 ]
+                , badge_href_url  = badge[ 1 ];
+
+              badge_li.classList.add( 'badge-list-item' );
+              badge_link.classList.add( 'badge-link' );
+
+              badge_link.setAttribute(
+                'href'
+              , badge_href_url
+              );
+
+              badge_image.setAttribute(
+                'src'
+              , badge_image_url
+              );
+
+              badge_link.appendChild( badge_image );
+              badge_li.appendChild( badge_link );
+              badge_list.appendChild( badge_li );
+            } );
+          }
+
+          link.innerText = 'link';
+          header.innerText = repo.name;
+          li.appendChild( header );
+          li.appendChild( link );
+          li.appendChild( desc );
+          li.appendChild( badge_list );
+          container.appendChild( li );
+        } );
+      } else {}
+    }
+
+    request.onerror = function(){};
+    request.send();
+  };
+
+  init();
 })();
